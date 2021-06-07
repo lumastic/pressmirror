35d5311a7d7167f4925a6685ae05a843c3e8ed50
Drew Lytle <aml3ed@virginia.edu>
2021-06-07 15:23:27 -0400

    feat(Menu): Make format menus render and toggle based on state
    Used the new useEditorContext hook to provide update handlers that
    trigger rerenders whenever the editor is updated.

--- src/components/FloatingMenu/FloatingMenu.tsx
+++ src/components/FloatingMenu/FloatingMenu.tsx
@@ -1,7 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { toggleLink } from "../../commands/toggleLink";
 import toggleMarkCommand from "../../commands/toggleMarkCommand";
-import { useProviderContext } from "../../core/context/useEditorContext";
+import {
+  useEditorContext,
+  useProviderContext
+} from "../../core/context/useEditorContext";
+import { Modal } from "lumastic-ui";
 import { FormatButton } from "../FormatButton";
 import style from "./FloatingMenu.scss";
 
@@ -9,64 +13,68 @@ type FloatingMenuProps = {
   children?: React.ReactNode;
 };
 
-const FloatingMenu = ({ children }: FloatingMenuProps): React.ReactElement => {
+const FloatingMenu = (): React.ReactElement => {
   const { viewProvider } = useProviderContext();
+  const { isInitialized } = viewProvider;
   const menuRef = useRef();
-  useEffect(() => {
-    function displayMenu() {
-      const view = viewProvider.editorView;
-      const state = view.state;
-      if (!menuRef.current || !state) return;
-      console.log("Henlo");
-      const { from, to } = state.selection;
-      const menu = menuRef.current as HTMLElement;
-      if (
-        view.hasFocus() &&
-        view.state.selection.$anchor !== view.state.selection.$head
-      ) {
-        try {
-          const start = view.coordsAtPos(from);
-          const end = view.coordsAtPos(to);
-          const box = menu.getBoundingClientRect();
-          const offsetParentBox = menu.offsetParent.getBoundingClientRect();
-          let left =
-            (start.left + end.left) / 2 - box.width / 2 - offsetParentBox.left;
-          if (left < 5) {
-            left = 5;
-          }
-          menu.style.left = left + "px";
-          menu.style.top =
-            start.top - offsetParentBox.top - box.height - 7 + "px";
-          menu.style.opacity = "1";
-        } catch {
-          console.error("FloatingMenu couldn't calculate it's position");
+  const displayMenu = useCallback(() => {
+    const view = viewProvider.editorView;
+    const state = view.state;
+    if (!menuRef.current || !state) return;
+    const { from, to } = state.selection;
+    const menu = menuRef.current as HTMLElement;
+    if (
+      view.hasFocus() &&
+      view.state.selection.$anchor !== view.state.selection.$head
+    ) {
+      try {
+        const start = view.coordsAtPos(from);
+        const end = view.coordsAtPos(to);
+        const box = menu.getBoundingClientRect();
+        const offsetParentBox = menu.offsetParent.getBoundingClientRect();
+        let left =
+          (start.left + end.left) / 2 - box.width / 2 - offsetParentBox.left;
+        if (left < 5) {
+          left = 5;
         }
-      } else {
-        menu.style.opacity = "0";
+        menu.style.left = left + "px";
+        menu.style.top =
+          start.top - offsetParentBox.top - box.height - 7 + "px";
+        menu.style.opacity = "1";
+        // menu.style.display = "block";
+      } catch {
+        console.error("FloatingMenu couldn't calculate it's position");
       }
+    } else {
+      // menu.style.display = "none";
+      menu.style.left = -100000 + "px";
+      menu.style.top = -100000 + "px";
+      menu.style.opacity = "0";
     }
-    console.log("Hello");
-    displayMenu();
-  }, [viewProvider.editorView.state]);
+  }, [viewProvider]);
+  useEditorContext(displayMenu);
+  if (!isInitialized) return null;
   return (
-    <div
-      className={style.floatingmenu}
-      data-testid={"floatingmenu"}
-      ref={menuRef}
-    >
-      <FormatButton command={toggleMarkCommand("strong")} mark={"strong"}>
-        B
-      </FormatButton>
-      <FormatButton command={toggleMarkCommand("underline")} mark="underline">
-        <u>U</u>
-      </FormatButton>
-      <FormatButton command={toggleMarkCommand("italic")} mark="italic">
-        <em>I</em>
-      </FormatButton>
-      <FormatButton command={toggleLink} mark="link">
-        L
-      </FormatButton>
-    </div>
+    <Modal isShowing>
+      <div
+        className={style.floatingmenu}
+        data-testid={"floatingmenu"}
+        ref={menuRef}
+      >
+        <FormatButton command={toggleMarkCommand("strong")} mark={"strong"}>
+          B
+        </FormatButton>
+        <FormatButton command={toggleMarkCommand("underline")} mark="underline">
+          <u>U</u>
+        </FormatButton>
+        <FormatButton command={toggleMarkCommand("italic")} mark="italic">
+          <em>I</em>
+        </FormatButton>
+        <FormatButton command={toggleLink} mark="link">
+          L
+        </FormatButton>
+      </div>
+    </Modal>
   );
 };
 
